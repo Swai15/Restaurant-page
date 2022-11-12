0e1f21d9cb9bfb93c0a8b90fc51193dc7caad871
Swai <swaisilvano5@gmail.com>
2022-11-12 12:33:20 +0300

    index.html webpack entry outpout test fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const { loadavg } = require("os");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  // entry: "./src/index.js",
+  entry: {
+    index: ".src/index.js",
+    print: "./src/homeTab.js",
+    pageLoad: "./src/page-load.js"
+
+  },
   output: {
-    filename: "main.js",
+    // filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname,"dist")
   },
   mode: "development"
